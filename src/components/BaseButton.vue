9796ca59f0990deb5755b84d64316ced0fc9e9e3
Nikita Zinovev <nikita.z@rcsmail.ru>
2019-11-15 00:18:43 +0300

    BaseButton component improvement

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+button.base-button(
+  :type="type"
+) {{ value }}
+</template>
+
+<script>
+export default {
+  props: {
+    type: String,
+    value: String
+  }
+};
+</script>
+
+<style lang="scss">
+.base-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 12px;
+  border-radius: 5px;
+  border: 1px solid rgba(67, 28, 93, 0.8);
+  background: rgba(90, 16, 148, 0.8);
+  font-family: 'HelveticaNeueMed';
+  font-size: 16px;
+  line-height: 19px;
+  color: #FFFFFF;
+  cursor: pointer;
+}
+</style>
